38715b4e279e27bf1b3bfdc2a381da03114d270c
Austin Keeley <austin.keeley@gmail.com>
2018-11-28 20:55:27 -0500

    cleaning up

--- entrance.py
+++ entrance.py
@@ -1,42 +1,52 @@
 """Entrance song"""
 
-from scapy.all import *
+from scapy.all import Ether, DHCP, sniff
 
 from music_player import MusicPlayer
 import data
 from util import log
 
-player = MusicPlayer()
 
-def dhcp_monitor_callback(pkt):
-    if not pkt.haslayer(DHCP):
-        return
-    if pkt[DHCP].options[0][1] != 3:
-        return
+class EntranceController(object):
+    """Class that starts listening for DHCP connections and playing music"""
+    def __init__(self):
+        self.player = MusicPlayer()
 
-    mac_addr = pkt[Ether].src
-    log('DHCP request from {}'.format(mac_addr))
-    device = data.get_device_by_mac_addr(mac_addr)
-    if not device:
-        log('This isn\'t a device I know about... Adding it to the database')
-        data.insert_device(mac_addr)
-        return
+    def start(self):
+        """Starts sniffing"""
+        sniff(prn=self.dhcp_monitor_callback, filter='udp and (port 67 or 68)', store=0)
 
-    if device.owner.song:
-        song = device.owner.song
-        log('{} is about to enter... playing {} by {}'.format(device.owner.name, song.title, song.artist))
-    else:
-        log('Device owner {} doesn\'t have a song. Doing nothing...'.format(device.owner.name))
-        return
 
-    uri, _ = player.search(song.artist, song.title)
-    if uri:
-        player.play_song(uri, duration=song.duration)
-    else:
-        log('No search results found...')
+    def dhcp_monitor_callback(self, pkt):
+        """Callback for DHCP requests"""
+        if not pkt.haslayer(DHCP):
+            return
+        if pkt[DHCP].options[0][1] != 3:
+            return
+
+        mac_addr = pkt[Ether].src
+        log('DHCP request from {}'.format(mac_addr))
+        device = data.get_device_by_mac_addr(mac_addr)
+        if not device:
+            log('This isn\'t a device I know about... Adding it to the database')
+            data.insert_device(mac_addr)
+            return
+
+        if device.owner.song:
+            song = device.owner.song
+            log('{} is about to enter! playing {} by {}'.format(device.owner.name, song.title, song.artist))
+        else:
+            log('Device owner {} doesn\'t have a song. Doing nothing...'.format(device.owner.name))
+            return
+
+        uri, _ = self.player.search(song.artist, song.title)
+        if uri:
+            self.player.play_song(uri, duration=song.duration)
+        else:
+            log('No search results found...')
 
 
 
 if __name__ == '__main__':
-    log('Entrance song')
-    sniff(prn=dhcp_monitor_callback, filter='udp and (port 67 or 68)', store=0)
+    entrance = EntranceController()
+    entrance.start()
